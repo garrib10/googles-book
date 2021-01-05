6b92548c0147a0aabe68339578e2ffe64d4ebdc0
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-01-04 19:44:52 -0500

    controllers folders updated

--- routes/api/books.js
+++ routes/api/books.js
@@ -4,12 +4,11 @@ const booksController = require("../../controllers/booksController");
 // Matches with "/api/books"
 router.route("/")
   .get(booksController.findAll)
-  .post(booksController.save);
+  .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router.route("/:id")
   .get(booksController.findOne)
-  
   .delete(booksController.remove);
 
 module.exports = router;
