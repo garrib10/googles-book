b6e1cf0756ca40c230a49d5e0911f872e2a2a945
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-01-07 00:24:43 -0500

    footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => (
+    <footer className="page-footer">
+		<div className="container-fluid text-center">
+			<p>Copyright 2021 Brian Garrity</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
