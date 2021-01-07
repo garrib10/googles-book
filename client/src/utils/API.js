d7c9ba75dcd052f029a182bc48585cc8794b4ddb
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-01-07 00:36:16 -0500

    added google api to uitls apijs

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,10 @@
 import axios from "axios";
 
 export default {
+   // Get book from google search 
+   getGoogleSearchBooks: function(query) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+},
   // Gets all books
   getBooks: function() {
     return axios.get("/api/books");
@@ -10,14 +14,14 @@ export default {
   getBook: function(id) {
     return axios.get("/api/books/" + id);
   },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  },
   // Deletes the book with the given id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
 };
 
 
