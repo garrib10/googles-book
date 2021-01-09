609864bf9ee1f757ea713eef8acf96b60b287e9f
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-01-09 18:41:01 -0500

    made changes

--- routes/api/books.js
+++ routes/api/books.js
@@ -9,6 +9,7 @@ router.route("/")
 // Matches with "/api/books/:id"
 router.route("/:id")
   .get(booksController.findById)
+  .put(bookController.update)
   .delete(booksController.remove);
 
 module.exports = router;
